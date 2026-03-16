f8069c086a552e1cb5bf6f5811b94d67722ae95f
Saurav Paul <sauravpaul.sunny@gmail.com>
2026-03-16 22:22:20 +0200

    (feat): add leader+fa to find all files including gitignored

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fa", function()
+  require("telescope.builtin").find_files({
+    no_ignore = true,
+    file_ignore_patterns = { "%.venv/", "node_modules/", "%.git/" },
+  })
+end, { desc = "Find All Files (incl. gitignored)" })
