2be8d6a41ba845815d0f79655bc2bc69196b775f
Saurav Paul <sauravpaul.sunny@gmail.com>
2026-03-16 22:59:21 +0200

    (chore): set catppuccin-mocha as colorscheme, remove example plugin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,16 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     opts = {
       flavour = "mocha",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
